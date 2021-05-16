78bb418da55930cac49cb713087011d4b19bb70a
Paulius B <Paulspremium@gmail.com>
2021-05-16 14:32:33 +0300

    edit

--- CodeWars/ageDiff.js
+++ CodeWars/ageDiff.js
@@ -14,5 +14,5 @@ function differenceInAges(ages){
         return [youngest, oldest, oldest-youngest];
     } 
     
-    console.log(differenceInAges([82, 15, 6, 38, 35]), `->`, [6, 82, 76]);
-    console.log(differenceInAges([57, 99, 14, 32]), `->`, [14, 99, 85]);
+console.log(differenceInAges([82, 15, 6, 38, 35]), `->`, [6, 82, 76]);
+console.log(differenceInAges([57, 99, 14, 32]), `->`, [14, 99, 85]);
